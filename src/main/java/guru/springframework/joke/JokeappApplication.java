04bbeb2cc42ea6f140f31a5aa4fba040e3fe9979
NOURHIRA El Mokhtar <enourhira@gmail.com>
2019-01-08 16:36:34 +0100

    commit for webhook

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource("classpath:chuck-config.xml")
 public class JokeappApplication {
-
+//small change here
 	public static void main(String[] args) {
 		SpringApplication.run(JokeappApplication.class, args);
 	}
